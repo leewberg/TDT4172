4d2cb100a1e091d4181758fc3e7fb3ac4d99ca0d
Lee Wesenberg <leewesenberg@gmail.com>
2025-09-05 21:20:34 +0200

    Mission 1 complete

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -2,16 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#hvilke formler kan jeg bruke:
-# z = SUM(w_i x_i + b, i = 1, ..., n) = wx + b
-# sigmoid: sigma(z) = 1/(1 + e^-z)
-# y_pred = 1/(1 + e -^(SUM(w_i * x_i +b), i = 1, ..., n))
-# performance measure P: p(y|x) = y_pred^y (1 - y_pred)^(1-y) (gjelder hovedsakelig om y skal være binært)
-# tapsfunksjon: L(y_pred, x) = -ln(p|x). modell defineres av w og b. må da finne parameterne som minimerer tapet i gjennomsnitt over alle datapunktene. mao: vil velge parameterne theta som maksimerer log-likelihood for target-verdiene y for hvert datapunkt x
-# derivert av L mhp theta: dL/
-# oppdatering av theta:
-# theta(t+1) = theta(t) -n* dL(f(x;theta), y)/d theta
-#  n: læringsrate 
 
 class LinearRegression():
     
@@ -20,33 +10,23 @@ class LinearRegression():
         # (with defaults) as you see fit
         self.learning_rate = learning_rate
         self.epochs = epochs
-        self.weights, self.bias = None, None
+        self.weights, self.bias, self.X_min, self.X_maks = None, None, None, None
         self.losses, self.train_accuracies = [], []
     
-    def _sigmoid(self, lin_model):
-        s = 1/(1+ np.e**(-lin_model))
-        return s
+    def normalize(self, X):
+        if isinstance(X, pd.Series):
+            X = X.values
+        else:
+            X=X
+        if X.ndim == 1:
+            X = X.reshape(-1, 1)
+        else:
+            X=X
 
-    def compute_gradients(self, x, y, y_pred):
-        grad_w = (y_pred-y)*x
-        grad_b = (y_pred-y)
-        return grad_w, grad_b
-
-    def compute_loss(self, y, y_pred):
-        L = -y*np.log(y_pred)- (1-y)*np.log(1-y_pred)
-        return L
-
-    def update_parameters(self, grad_w, grad_b):
-        for i in range (len(self.weights)):
-            s = self.weights[i]-self.learning_rate*grad_w[i]
-            print(s)
-            self.weights[i] = s
-        self.bias = self.bias - self.learning_rate*grad_b
-
-    def accuracy(self, true_values, predictions):
-        return np.mean(true_values == predictions)
+        X = (X - self.X_min)/(self.X_maks - self.X_min)
+        return X
         
-    def fit(self, x: list, y):
+    def fit(self, X, y):
         """
         Estimates parameters for the classifier
         
@@ -55,24 +35,23 @@ class LinearRegression():
                 m rows (#samples) and n columns (#features)
             y (array<m>): a vector of floats
         """
-        self.weights = x #x.shape = [#samples, #features]
+        self.X_min = X.min(axis = 0)
+        self.X_maks = X.max(axis=0)
+        X = self.normalize(X)
+        rows, cols = X.shape
+        self.weights = np.zeros(cols)
         self.bias = 0
         #Gradient Descent
         for _ in range (self.epochs):
-            lin_model = np.matmul(self.weights, x) + self.bias
-            y_pred = self._sigmoid(lin_model)
-            grad_w, grad_b = self.compute_gradients(x, y, y_pred)
-            self.update_parameters(grad_w, grad_b)
-
-            loss = self.compute_loss(y, y_pred)
-            pred_to_class = 1 if y > 0.5 else 0 #problem child :(
-            self.train_accuracies.append(self.accuracy(y, pred_to_class))
-            self.losses.append(loss)
-            
+            lin_model = np.dot(X, self.weights)+self.bias
+            diff_w = (1/rows) *np.dot(X.T, (lin_model-y))
+            diff_b = (1/rows) * np.sum(lin_model - y)
+            self.weights = self.weights - self.learning_rate*diff_w
+            self.bias = self.bias - self.learning_rate*diff_b            
         
 
     
-    def predict(self, x):
+    def predict(self, X):
         """
         Generates predictions
         
@@ -85,26 +64,6 @@ class LinearRegression():
         Returns:
             A length m array of floats
         """
-        lin_model = np.matmul(x, self.weights) + self.bias
-        y_pred = self._sigmoid(lin_model)
-        return [1 if y>0.5 else 0 for y in y_pred]
-
-
-
-
-data = pd.read_csv('mission1.csv')
-
-plt.figure(figsize=(6, 4))
-plt.scatter(data['Net_Activity'], data['Energy'], c='blue', label='Data points')
-plt.grid(True)
-plt.xlabel('Network Activity', fontsize=14)
-plt.ylabel('Energy', fontsize=14)
-plt.title('Energy vs. Traffic', fontsize=16)
-plt.legend()
-#plt.show()
-
-lr = LinearRegression()
-
-lr.fit(data['Net_Activity'], data['Energy'])
-
-lr.predict(data['Net_Activity'])
+        X = self.normalize(X)
+        lin_model = np.dot(X, self.weights) + self.bias
+        return lin_model
